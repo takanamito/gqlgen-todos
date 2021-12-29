2586e731654b9b5cef4cfb135db10fd1a6f1ad7a
takanamito <takanamito0928@gmail.com>
2021-12-29 15:50:07 +0900

    Create sample data

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/takanamito/gqlgen-todos/ent"
 	"github.com/takanamito/gqlgen-todos/ent/user"
@@ -21,6 +22,7 @@ func main() {
 	if err := client.Schema.Create(context.Background()); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
+	CreateTodos(context.Background(), client)
 }
 
 func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
@@ -48,4 +50,38 @@ func QueryUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
 	}
 	log.Println("user returned: ", u)
 	return u, nil
+}
+
+func CreateTodos(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	todo, err := client.Todo.
+		Create().
+		SetBody("掃除をする").
+		SetCreatedAt(time.Now()).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating todo: %w", err)
+	}
+	log.Println("todo was created: ", todo)
+
+	todo2, err := client.Todo.
+		Create().
+		SetBody("買い物に行く").
+		SetCreatedAt(time.Now()).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating todo: %w", err)
+	}
+	log.Println("todo was created: ", todo2)
+
+	takanamito, err := client.User.
+		Create().
+		SetAge(30).
+		SetName("takanamito").
+		AddTodos(todo, todo2).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %w", err)
+	}
+	log.Println("user was created: ", takanamito)
+	return takanamito, nil
 }
