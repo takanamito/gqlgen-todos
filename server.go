3b09c6769de62fe4400f43238bff058f11c60b5e
takanamito <takanamito0928@gmail.com>
2022-01-10 22:36:41 +0900

    Migrate on start up server

--- server.go
+++ server.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/lib/pq"
+
+	"github.com/takanamito/gqlgen-todos/ent"
 	"github.com/takanamito/gqlgen-todos/graph"
 	"github.com/takanamito/gqlgen-todos/graph/generated"
 )
@@ -14,6 +18,18 @@ import (
 const defaultPort = "8080"
 
 func main() {
+	// マイグレーション
+	client, err := ent.Open("postgres", "host=localhost port=5432 user=admin dbname=todos password=admin sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	if err := client.Debug().Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	// サーバーの初期化
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
